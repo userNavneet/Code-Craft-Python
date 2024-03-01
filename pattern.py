2cb32ed6debd92d780adeaf40e729544803a9860
Navneet <mailsharmanavneet@gmail.com>
2024-03-01 11:46:24 +0530

    print pattern

---
+++ pattern.py
@@ -0,0 +1,8 @@
+def print_pattern(rows):
+    for i in range(1, rows + 1):
+        for j in range(1, i + 1):
+            print("#", end=" ")
+        print()
+rows = int(input("Enter the number of rows for the pattern: "))
+print_pattern(rows)
+   
