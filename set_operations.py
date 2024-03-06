dfaa6d1b3ff97159ec80fda0504e1059636a42a7
Navneet <mailsharmanavneet@gmail.com>
2024-03-06 11:13:56 +0530

    set operations

---
+++ set_operations.py
@@ -0,0 +1,10 @@
+# set operations
+A = {1,2,3,4,5,6,7,8}
+B = {2,4,6,8}
+print("A = ",A)
+print("B = ",B)
+print("various set operations on A and B")
+print("A union B is: ",A | B)
+print("A intersaction B is: ",A & B)
+print("Difference of sets A and B: ",A - B)
+print("Symmetric difference of A and B: ",A ^ B)
