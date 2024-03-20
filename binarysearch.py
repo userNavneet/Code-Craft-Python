53855e852bf7ec2c640bff6f092cfcfccce1b037
Navneet <mailsharmanavneet@gmail.com>
2024-03-20 11:25:08 +0530

    binary search opoeration on sorted array

---
+++ binarysearch.py
@@ -0,0 +1,35 @@
+def binary_search(arr, x):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        # Check if x is present at mid
+        if arr[mid] == x:
+            return mid
+        # If x is greater, ignore left half
+        elif arr[mid] < x:
+            left = mid + 1
+        # If x is smaller, ignore right half
+        else:
+            right = mid - 1
+    # If element is not present in array
+    return -1
+
+# Get user input for the array
+n = int(input("Enter the number of elements in the array: "))
+arr = []
+print("Enter the elements of the sorted array:")
+for _ in range(n):
+    arr.append(int(input()))
+
+# Get user input for the element to search
+x = int(input("Enter the element to search: "))
+
+# Perform binary search
+result = binary_search(arr, x)
+
+# Print the result
+if result != -1:
+    print("Element is present at index", result)
+else:
+    print("Element is not present in array")
