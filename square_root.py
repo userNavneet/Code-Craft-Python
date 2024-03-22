51f8ea2c71a0dce6b26c218409b291ce9d223fb8
Navneet <mailsharmanavneet@gmail.com>
2024-03-22 20:27:05 +0530

    findind square root of a number

---
+++ square_root.py
@@ -0,0 +1,11 @@
+#using the math.sqrt() function from the math module.
+import math
+
+# Get user input for the number
+number = float(input("Enter a number: "))
+
+# Calculate the square root
+result = math.sqrt(number)
+
+# Print the result
+print("The square root of", number, "is:", result)
