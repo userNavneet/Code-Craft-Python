3b0bc3005b63faa4f468c650c9ab9db8fc5afec2
Navneet <mailsharmanavneet@gmail.com>
2024-03-22 20:35:18 +0530

    printing first n prime numbers

---
+++ n_prime_numbers.py
@@ -0,0 +1,29 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    elif num <= 3:
+        return True
+    elif num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def print_n_primes(n):
+    count = 0
+    num = 2
+    while count < n:
+        if is_prime(num):
+            print(num, end=" ")
+            count += 1
+        num += 1
+
+# Get user input for the value of 'n'
+n = int(input("Enter the value of 'n': "))
+
+# Print the first 'n' prime numbers
+print("The first", n, "prime numbers are:")
+print_n_primes(n)
