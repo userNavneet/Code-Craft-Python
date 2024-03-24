96720d7482d72b9f8a8f011d08420dcf62070b0c
Navneet <mailsharmanavneet@gmail.com>
2024-03-24 21:27:38 +0530

    delete the list

---
+++ delete_list.py
@@ -0,0 +1,11 @@
+# Create the list
+L = [10, 20, 30]
+
+# Print the initial list
+print("Initial List:", L)
+
+# Delete the list
+del L
+
+# Attempting to print the list after deletion will result in an error
+# print(L)
