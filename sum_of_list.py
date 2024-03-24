1a272d431a4f5b3fcd2aef58f4dd9ed277fe616e
Navneet <mailsharmanavneet@gmail.com>
2024-03-24 21:28:20 +0530

    sum of all the numbers in the list

---
+++ sum_of_list.py
@@ -0,0 +1,11 @@
+# Create the list
+L = [10, 20, 30]
+
+# Print the initial list
+print("Initial List:", L)
+
+# Calculate the sum of all numbers in the list
+sum_of_numbers = sum(L)
+
+# Print the sum
+print("Sum of all numbers in the list:", sum_of_numbers)
