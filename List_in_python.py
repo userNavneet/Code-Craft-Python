da9d0550d4c4b2c8ae1f2dc34e821056881cda80
Navneet <mailsharmanavneet@gmail.com>
2024-03-24 21:27:02 +0530

    list in python

---
+++ List_in_python.py
@@ -0,0 +1,14 @@
+# Create the list
+L = [10, 20, 30]
+
+# Print the initial list
+print("Initial List:", L)
+
+# Ask the user to input a new number
+new_number = int(input("Enter a new number to insert into the list: "))
+
+# Insert the new number into the list
+L.append(new_number)
+
+# Print the updated list
+print("Updated List:", L)
