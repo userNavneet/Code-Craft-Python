3c231dfbe4da6a952f8bc1f9c6c5232bc34b902a
Navneet <mailsharmanavneet@gmail.com>
2024-03-25 23:34:00 +0530

    printing pyramid pattern

--- pyramid.py
+++ pyramid.py
@@ -2,5 +2,5 @@ def print_pyramid(rows):
     for i in range(1, rows + 1):
         print(" " * (rows - i) + "* " * i)
 
-rows = int(input("Enter the number of rows for the pyramid: "))
+rows = int(input("Please Enter the number of rows for the pyramid: "))
 print_pyramid(rows)
