697c358db3065a3c6a2bd300556fff81df7dc683
Navneet <mailsharmanavneet@gmail.com>
2024-03-28 13:11:37 +0530

    linear search in an array

--- linear_search.py
+++ linear_search.py
@@ -1,21 +1,3 @@
-# import array
-# def linearSearch(array, n, x):
-#     for i in range(0, n):
-#         if (array[i]==x):
-#             return i
-#     array=[]
-# n = int(input("Enter the length of list: "))
-# print("Enter the ",n,"elements")
-# for i in range(0,n):
-#     value=int(input())
-#     array.append(value)
-# key=int(input("Enter the value you want to search:"))
-# result=linearSearch(array, n, key)
-# if(result == -1):
-#     print("Element not found")
-# else:
-#     print("Element found at index: ",result)    
-
 def linear_search(arr, target):
     for i in range(len(arr)):
         if arr[i] == target:
@@ -26,7 +8,7 @@ def linear_search(arr, target):
 arr = list(map(int, input("Enter the elements of the array separated by space: ").split()))
 
 # Get the target value from the user
-target = int(input("Enter the value to search for: "))
+target = int(input("Enter the element you want to search: "))
 
 # Perform linear search
 result = linear_search(arr, target)
