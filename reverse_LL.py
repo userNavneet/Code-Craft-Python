4f681eeb35525321df12178fb44c4db48079025f
Navneet <mailsharmanavneet@gmail.com>
2024-04-03 11:19:49 +0530

    reversing a linked list

---
+++ reverse_LL.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end=" -> ")
+            current_node = current_node.next
+        print("None")
+
+    def reverse(self):
+        prev_node = None
+        current_node = self.head
+        while current_node:
+            next_node = current_node.next
+            current_node.next = prev_node
+            prev_node = current_node
+            current_node = next_node
+        self.head = prev_node
+
+# Example usage:
+ll = LinkedList()
+for i in range(1, 6):
+    ll.append(i)
+
+print("Original list:")
+ll.print_list()
+
+ll.reverse()
+
+print("\nReversed list:")
+ll.print_list()
