06d6f71d3d2d6a687a921dfdb61aaaf5f5b27bf0
Navneet <mailsharmanavneet@gmail.com>
2024-04-04 17:18:53 +0530

    using oops in python

---
+++ oops.py
@@ -0,0 +1,11 @@
+class Person():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def __str__(self):
+        return f"{self.name}({self.age})"
+    def myfun(self):
+        print("My name is :" + self.name)
+        # print("I am " + self.age)
+p1 = Person("Lalit", 22)
+p1.myfun()    
