45e06da90fb00616838d92f7a36e3188a1983344
Navneet <mailsharmanavneet@gmail.com>
2024-04-09 11:32:22 +0530

    exercise log 2: modified the user input program

--- exersice.py
+++ exersice.py
@@ -1,9 +1,9 @@
 patient_name=input("enter patient name: ")
 patient_age=input("enter patient age: ")
-is_patient_new=input("are you a new patient: enter 'True' or 'False'")
+is_patient_new=input("are you a new patient: enter 'True' or 'False' ")
 boolian_input = bool(is_patient_new)
-print(patient_name,' ',patient_age,' ',is_patient_new)
 if(boolian_input == True):
-    print(patient_name,"is a new patient")
+    print(patient_name + " is a new patient")
+    print(patient_age +' years old')
 else:
-    print(patient_name,"is an old patient")
+    print(patient_name + " is an old patient")
