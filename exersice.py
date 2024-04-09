481ea09e30b32d7fc9dec156f3f4f9943e87fd53
Navneet <mailsharmanavneet@gmail.com>
2024-04-09 22:52:26 +0530

    log 4: concatenating two strings by using formatted strings

--- exersice.py
+++ exersice.py
@@ -1,3 +1,7 @@
-name = input('Enter your name: ')
-color = input('Enter your favourite color: ')
-print(name + " likes " + color + " color")
+brand = 'Mercedes Benz'
+model = 'GLE 53 Coupe'
+#concatenating two strings by using formatted strings
+message = f'{brand}[{model}] is a great car'
+
+# message = brand + '[' + model + '] is a great car'
+print(message) 
