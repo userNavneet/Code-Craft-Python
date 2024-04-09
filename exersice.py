f8f27eafb05fa324785312408c75351ffa24358c
Navneet <mailsharmanavneet@gmail.com>
2024-04-09 11:54:04 +0530

    exercise log 3: combining user inputs and printing them

--- exersice.py
+++ exersice.py
@@ -1,9 +1,3 @@
-patient_name=input("enter patient name: ")
-patient_age=input("enter patient age: ")
-is_patient_new=input("are you a new patient: enter 'True' or 'False' ")
-boolian_input = bool(is_patient_new)
-if(boolian_input == True):
-    print(patient_name + " is a new patient")
-    print(patient_age +' years old')
-else:
-    print(patient_name + " is an old patient")
+name = input('Enter your name: ')
+color = input('Enter your favourite color: ')
+print(name + " likes " + color + " color")
