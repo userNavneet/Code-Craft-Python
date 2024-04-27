06c32ed2a2f8d696155de1a5b99d9f12f63c44a4
Navneet <mailsharmanavneet@gmail.com>
2024-04-27 13:37:56 +0530

    example of class and object

--- oops.py
+++ oops.py
@@ -6,6 +6,6 @@ class Person():
         return f"{self.name}({self.age})"
     def myfun(self):
         print("My name is :" + self.name)
-        # print("I am " + self.age)
-p1 = Person("Lalit", 22)
+        print("I am " + self.age)
+p1 = Person("Lalit", "22")
 p1.myfun()    
